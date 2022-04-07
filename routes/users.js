6cd0752bf0a09d3a4660c349662d8e236b3a007d
Rúben Martins <rubenbatistamartins@gmail.com>
2022-04-07 23:24:37 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/estaticos', function(req, res, next) {
   let users=[{name:"John Doe", birthDate:"19/02/1999"}];
   res.send(users);
  });
  
 
 module.exports = router;
-/*
+
 //Pode estar ou não correto:
 const db = require("./models/connection")
 router.get('/teste', function(req, res, next) {
       db.any("SELECT * FROM utilizador")
-      .then(rows => {
-        console.log(rows);
-        res.json(rows)
-      })
-      .catch(error => {
-        console.log(error)
-      })
- });*/
+      res.send(users);
+ });
