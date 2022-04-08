f91d07a64cb227b9be67f6e3ed4e8953435893bb
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-04-08 01:45:00 +0100

    CH50

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/estaticos', function(req, res, next) {
+  let users=[{name:"John Doe", birthDate:"19/02/1999"}];
+  res.send(users);
+ });
+ 
+
+module.exports = router;
+
+/*
+router.get('/teste', function(req, res, next) {
+  pool.query('SELECT * FROM utilizador', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+});*/
