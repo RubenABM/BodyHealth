498fd1f36b3940ad129f55a8b5be14e72ab87c84
Rúben Martins <rubenbatistamartins@gmail.com>
2022-04-16 00:01:30 +0100

    123

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const pool = require('./models/connection.js')
 
 app.get("/", (req, res) => {
 
     res.send("Hello World")
 
 });
+
+pool.connect();
 
 app.listen(port, () => console.log('App is online in ${port}'));
