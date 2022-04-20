2cc29ab812afedd03ca5513b8d81199f881ebf0e
Rúben Martins <rubenbatistamartins@gmail.com>
2022-04-20 11:26:05 +0100

    111

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 
 
-//var express = require('express');
-//var router = express.Router();
-//const pool = require('./models/connection.js')
+var express = require('express');
+var router = express.Router();
+const pool = require('./models/connection.js')
 
 /* GET users listing. */
-/*router.get('/estaticos', function(req, res, next) {
+router.get('/estaticos', function(req, res, next) {
   let users=[{name:"John Doe", birthDate:"19/02/1999"}];
   res.send(users);
 });
@@ -30,6 +30,6 @@ router.get('/homepagetext', (req, res) => {
 })
 
 module.exports = router;
-*/
+
 
 
