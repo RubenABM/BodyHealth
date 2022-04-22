e17448b57235268ee6f8c117325725f74c0202c5
Rúben Martins <rubenbatistamartins@gmail.com>
2022-04-22 10:12:30 +0100

    123

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,22 @@
-function change() {
+/*function change() {
     var x = document.getElementsByClassName("marker");
-    if (x.style.visibility === "visible") {
+    if (x.style.visibility ==  "visible") {
         x.style.visibility = "hidden";
     } else {
         x.style.visibility = "visible";
-    }
-}
+    }       
+}*/
+
+function change(){
+    [].forEach.call(document.querySelectorAll(".marker"), function(element) {
+      if(element.style.visibility == "visible")
+      {
+        element.style.visibility = "hidden";
+      }
+      else if(element.style.visibility == "hidden")
+      {
+        element.style.visibility = "visible";
+      }
+    });
+
+  }
