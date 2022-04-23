07ce75860a528500f9d005529246be50ff8693c7
RubenABM <rubenbatistamartins@gmail.com>
2022-04-23 23:59:19 +0100

    Update connection.js

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,8 @@
-/*var pg = require('pg');
+var pg = require('pg');
 
-//pode estar mal!
 const connectionString = "postgres://zrznopdgddhzpb:c25ef89e544d07ff6382509ce1ec20a4535bffede8a2d96367c35ec1910e88b2@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d8sb04nd3dc7fk"
 //"postgres://root:password@localhost:5432/students"
+
 const Pool = pg.Pool
 const pool = new Pool({
  connectionString,
@@ -13,27 +13,9 @@ const pool = new Pool({
  }
 })
 
-pool.connect()
-pool.query('SELECT * FROM utilizador', (err,res)=>{
-    console.log(err,res)
-    //pool.end()
-})
-
-module.exports = ool;*/
-
-const {Client} = require('pg');
-
-const client = new Client({
-    host: "localhost",
-    user: "zrznopdgddhzpb",
-    password: "c25ef89e544d07ff6382509ce1ec20a4535bffede8a2d96367c35ec1910e88b2",
-    database : 'd8sb04nd3dc7fk',
-    port: '5432'
-}); 
- 
-client.connect(function(err) {
+pool.connect(function(err) {
   if (err) throw err;
-  console.log('Database is connected successfully !')
+  console.log('Database is connected successfully !');
 });
 
-module.exports = client;
+module.exports = pool;
