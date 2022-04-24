db26c430227be3a36aefc4d848eb06049605463c
RubenABM <rubenbatistamartins@gmail.com>
2022-04-25 00:22:21 +0100

    123

--- server/server.js
+++ server/server.js
@@ -27,8 +27,11 @@ app.get('/users', function(req, res) {
   });
 });
 
-app.post('/user-details', function(req, res) {
-  db.query('SELECT * FROM users WHERE email = "' + req.body.email + '" AND password = "' + req.body.password + '"',
+//Tentaiva de registo
+app.post('/register', function(req, res) {
+  db.query('insert into utilizador(user_name,user_password,user_morada,user_email,user_points,user_admin,user_pt,user_nutri)' +
+  'values (' + req.body.name + ','+ req.body.password + ',' + req.body.email + ','  + req.body.morada + '0, "0", "0","0")',
+    //'SELECT * FROM users WHERE email = "' + req.body.email + '" AND password = "' + req.body.password + '"',
     function(err, rows, fields) {
       if (err) {
         res.json({ msg: 'error' });
@@ -38,6 +41,22 @@ app.post('/user-details', function(req, res) {
       }
   });
 });
+
+//Tentativa de login
+app.post('/login', function(req, res) {
+  db.query('SELECT * FROM utilizador WHERE email = "' + req.body.username + '" AND password = "' + req.body.password + '"',
+    function(err, rows, fields) {
+      if (err) {
+        res.json({ msg: 'error' });
+        alert("BOA!");
+      } 
+      else {
+        res.json({  msg: 'success', user: rows });
+        alert("MAU!");
+      }
+  });
+});
+
 
 app.listen(8080, function() {
     console.log('Node app is running on port 8080');
