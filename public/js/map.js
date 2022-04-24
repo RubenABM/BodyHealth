2823125f93e35e3610433516c2fea58683d865d3
RubenABM <rubenbatistamartins@gmail.com>
2022-04-24 16:50:53 +0100

    123

---
+++ public/js/map.js
@@ -0,0 +1,22 @@
+function change() {
+    var x = document.getElementsByClassName("marker");
+    if (x.style.visibility ==  "visible") {
+        x.style.visibility = "hidden";
+    } else {
+        x.style.visibility = "visible";
+    }       
+}
+
+function change2(){
+    [].forEach.call(document.querySelectorAll(".marker"), function(element) {
+      if(element.style.visibility == "visible")
+      {
+        element.style.visibility = "hidden";
+      }
+      else if(element.style.visibility == "hidden")
+      {
+        element.style.visibility = "visible";
+      }
+    });
+
+  }
---
+++ server/server.js
@@ -0,0 +1,41 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var db = require('./database');
+var cors = require('cors');
+ 
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const corsOpts = { origin: '*', methods: ['GET', 'POST'], allowedHeaders: ['Content-Type']};
+app.use(cors(corsOpts));
+
+app.get('/users', function(req, res) {
+  db.query('SELECT * FROM utilizador', function(err, rows) {
+    if (err) {
+      res.json({ msg: 'error'});
+    } else {
+      res.json({ msg: 'success', users: rows });
+    }
+  });
+});
+
+app.post('/user-details', function(req, res) {
+  db.query('SELECT * FROM users WHERE email = "' + req.body.email + '" AND password = "' + req.body.password + '"',
+    function(err, rows, fields) {
+      if (err) {
+        res.json({ msg: 'error' });
+      } 
+      else {
+        res.json({  msg: 'success', user: rows });
+      }
+  });
+});
+
+app.listen(8080, function() {
+    console.log('Node app is running on port 8080');
+});
+module.exports = app;
