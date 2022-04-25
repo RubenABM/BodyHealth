c995be71b2d9c99b3dae8813ecfaf488494d50da
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-04-25 02:24:49 +0100

    c

--- server/server.js
+++ server/server.js
@@ -27,7 +27,6 @@ app.get('/users', function(req, res) {
   });
 });
 
-<<<<<<< HEAD
 app.get('/categoriasreceita', function(req, res){
 
   db.query('SELECT * FROM place_category', function(err, rows) {
@@ -88,13 +87,6 @@ app.post('/sendcategory', function(req, res) {
 
 app.post('/user-details', function(req, res) {
   db.query('SELECT * FROM users WHERE email = "' + req.body.email + '" AND password = "' + req.body.password + '"',
-=======
-//Tentaiva de registo
-app.post('/register', function(req, res) {
-  db.query('insert into utilizador(user_name,user_password,user_morada,user_email,user_points,user_admin,user_pt,user_nutri)' +
-  'values (' + req.body.name + ','+ req.body.password + ',' + req.body.email + ','  + req.body.morada + '0, "0", "0","0")',
-    //'SELECT * FROM users WHERE email = "' + req.body.email + '" AND password = "' + req.body.password + '"',
->>>>>>> b0249ab26ec6432218999a10071c1e6d67cd2e0d
     function(err, rows, fields) {
       if (err) {
         res.json({ msg: 'error' });
@@ -105,23 +97,6 @@ app.post('/register', function(req, res) {
   });
 });
 
-<<<<<<< HEAD
-=======
-//Tentativa de login
-app.post('/login', function(req, res) {
-  db.query('SELECT * FROM utilizador WHERE email = "' + req.body.username + '" AND password = "' + req.body.password + '"',
-    function(err, rows, fields) {
-      if (err) {
-        res.json({ msg: 'error' });
-        alert("BOA!");
-      } 
-      else {
-        res.json({  msg: 'success', user: rows });
-        alert("MAU!");
-      }
-  });
-});
->>>>>>> b0249ab26ec6432218999a10071c1e6d67cd2e0d
 
 
 app.listen(8080, function() {
