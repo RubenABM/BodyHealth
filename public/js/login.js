0fdf4c968b53f86d40a9dd7022f242af08b1c7d2
RubenABM <rubenbatistamartins@gmail.com>
2022-04-25 12:47:51 +0100

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -53,6 +53,20 @@ $(document).ready(function() {
 
     getUsersList();
 
+    function getUsersList2() {
+        
+        $.ajax({
+            url: "https://bodyhealthweb.herokuapp.com/users",
+            type: "GET",
+            dataType: 'json',
+            success: function(result) {
+                console.log(result);
+            }
+        });
+    }
+
+    getUsersList2();
+
     $('#btnSubmit').on('click', function(event) {
         
         // prevent form default behaviour
