efa8a2ea5cbc12df569fc6ba8da51e1e3a29a005
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-04-27 10:45:50 +0100

    ch88

--- models/placesModel.js
+++ models/placesModel.js
@@ -12,3 +12,64 @@ module.exports.getPlaces = async function() {
         return { status: 500, data: err };
     }
 }
+
+module.exports.getPlace = async function(id){
+
+    console.log("[placesModel.getPlace] id = " + JSON.stringify(id));
+
+    try{
+
+        let sql =
+            "SELECT * " +
+            "FROM place p " +
+            "WHERE p.local_id = $1;";
+
+        let result = await pool.query(sql, [id]);
+
+        let places = result.rows;
+
+        if (places.length > 0) {
+            console.log("[placesModel.getPlace] place = " + JSON.stringify(places[0]));
+            return { status: 200, data: places[0] };
+        } else {
+            return { status: 404, data: { msg: "Place not found." } };
+        }
+
+    } catch (err){
+
+      console.log(err);
+      return {status: 500, data: err};
+
+    }
+
+}
+
+
+//POST METHOD
+
+module.exports.savePlace = async function(local){
+
+  
+    console.log("[placesModel.savePlace] local = " + JSON.stringify(local));
+
+    try {
+        let sql =
+            "INSERT " +
+            "INTO place " +
+            "(local_morada, local_category_id, local_nome, ref_system_id, geometry_info_point) " +
+            "VALUES ($1, $2, $3, $4, $5) " +
+            "RETURNING local_id";
+        let result = await pool.query(sql, [local.local_morada, local.local_category_id, local.local_nome, local.ref_system_id, local.geometry_info_point]);
+        let place = result.rows[0];
+        console.log("[placesModel.savePlace] place = " + JSON.stringify(place));
+        return { status: 200, data: place };
+    } catch (err) {
+        console.log(err);
+        if (err.errno == 23503) // FK error
+            return { status: 400, data: { msg: "Type not found" } };
+        else
+            return { status: 500, data: err };
+    }
+
+
+}
--- routes/places.js
+++ routes/places.js
@@ -9,4 +9,22 @@ router.get('/', async function(req, res, next) {
 
 });
 
+router.get('/:id(\\d+)', async function(req, res, next){
+
+  let id = req.params.id;
+  console.log("[placesRoutes] Retrieving place with id " + id);
+  let result = await placesModel.getPlace(id);
+  res.status(result.status).send(result.data);
+
+});
+
+router.post('/addplace', async function(req, res, next){
+
+  let newPlace = req.body;
+  console.log("[placesRoutes] Saving place " + JSON.stringify(newPlace));
+  let result = await placesModel.savePlace(newPlace);
+  res.status(result.status).send(result.data);
+
+});
+
 module.exports = router;
