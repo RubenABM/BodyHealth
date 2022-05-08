6ad301697c4e5ca2af72827b00e15a02157d39fb
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-05-08 02:40:46 +0100

    ch110

--- models/planosModel.js
+++ models/planosModel.js
@@ -1,3 +1,109 @@
 var pool = require("./database");
 
 //COPIAR MÉTODOS DOS EXERCICIOS
+
+module.exports.getPlanoById = async function(plano_id) {
+    try {
+        let sql = "SELECT plano.plano_titulo, plano.plano_treino_desc, utilizador.user_name , item_aprovacao.tipoaprovacao_nome FROM plano " + "INNER JOIN utilizador ON utilizador.user_id = plano.plano_utilizador_id " + "INNER JOIN item_aprovacao ON item_aprovacao.aprovacao_tipo_id = plano.plano_treino_tipo_aprovacao_id " + "WHERE plano.plano_treino_id = " + plano_id;
+        let result = await pool.query(sql);
+        let planofound = result.rows;
+        console.log("[planosModel.getPlanoById] plano = " + JSON.stringify(planofound));
+        return { status: 200, data: planofound };
+    } catch (err) {
+        console.log(err);
+        return { status: 500, data: err };
+    }
+}
+
+
+module.exports.getPlanosUser = async function(uti_id) {
+    try {
+        let sql = "SELECT plano.plano_titulo, plano.plano_treino_desc, utilizador.user_name , item_aprovacao.tipoaprovacao_nome FROM plano " + "INNER JOIN utilizador ON utilizador.user_id = plano.plano_utilizador_id " + "INNER JOIN item_aprovacao ON item_aprovacao.aprovacao_tipo_id = plano.plano_treino_tipo_aprovacao_id " + "WHERE plano.plano_utilizador_id = " + uti_id;
+        let result = await pool.query(sql);
+        let planofound = result.rows;
+        console.log("[exercicioModel.getExercisesUser] planos = " + JSON.stringify(planofound));
+        return { status: 200, data: planofound };
+    } catch (err) {
+        console.log(err);
+        return { status: 500, data: err };
+    }
+}
+
+module.exports.savePlano = async function(plano) {
+    console.log("[recipesModel.saveRecipe] exercise = " + JSON.stringify(plano));
+    /* checks all fields needed and ignores other fields
+    if (typeof user != "object" || failUser(user)) {
+        if (user.errMsg)
+            return { status: 400, data: { msg: user.errMsg } };
+        else
+            return { status: 400, data: { msg: "Malformed data" } };
+    }*/
+    try {
+
+        let sql =
+            "INSERT " +
+            "INTO plano " +
+            "(plano_titulo, plano_treino_desc, plano_utilizador_id, plano_treino_aprovacao_pt, plano_treino_tipo_aprovacao_id) " +
+            "VALUES ($1, $2, $3, $4, $5) " +
+            "RETURNING plano_treino_id";
+
+            console.log(plano_titulo +"|"+ plano_treino_desc + "|" +  plano_utilizador_id + "|" + plano_treino_aprovacao_pt + "|" + plano_treino_tipo_aprovacao_id);
+        let result = await pool.query(sql, [plano_titulo, plano_treino_desc, plano_utilizador_id, plano_treino_aprovacao_pt, plano_treino_tipo_aprovacao_id]);
+        let planoooo = result.rows[0].plano_treino_id;
+        return { status: 200, data: planoooo };
+    } catch (err) {
+        console.log(err);
+        if (err.errno == 23503) // FK error
+            return { status: 400, data: { msg: "Type not found" } };
+        else
+            return { status: 500, data: err };
+    }
+}
+
+module.exports.getPlanosExercises = async function(plan_id) {
+    try {
+        let sql = "SELECT exercicio_plano.exercicio_plano_id, exercicio.exercicio_titulo, exercicio.exercicio_desc, exercicio.exercicio_num_series, exercicio.exercicio_num_repeticoes, exercicio_dificuldade.exercicio_dificuldade, exercicio_tipo.exercicio_tipo_titulo, utilizador.user_name FROM exercicio_plano " + "INNER JOIN exercicio ON exercicio.exercicio_id = exercicio_plano.exercise_id " + "INNER JOIN exercicio_dificuldade ON exercicio_dificuldade.exercicio_dificuldade_id = exercicio.exercicio_dificuldade_id " + "INNER JOIN exercicio_tipo ON exercicio_tipo.exercicio_tipo_id = exercicio.exercicio_tipo_id "+ "INNER JOIN utilizador ON utilizador.user_id = exercicio.exercicio_utilizador_id " + "WHERE exercicio_plano.p_treino_id = " + plan_id;
+        let result = await pool.query(sql);
+        let planofound = result.rows;
+        console.log("[exercicioModel.getExercisesUser] planos = " + JSON.stringify(planofound));
+        return { status: 200, data: planofound };
+    } catch (err) {
+        console.log(err);
+        return { status: 500, data: err };
+    }
+}
+
+
+//PRIMEIRA MARCACAO DE FAVORITO
+
+module.exports.savePlanoFavorite = async function(plano) {
+
+    console.log("[recipesModel.saveRecipe] favoriteplano = " + JSON.stringify(plano));
+    /* checks all fields needed and ignores other fields
+    if (typeof user != "object" || failUser(user)) {
+        if (user.errMsg)
+            return { status: 400, data: { msg: user.errMsg } };
+        else
+            return { status: 400, data: { msg: "Malformed data" } };
+    }*/
+    try {
+
+        let sql =
+            "INSERT " +
+            "INTO marcacao_favorito_plano " +
+            "(utilizador_id, plano_treino_id, is_plano_favorito) " +
+            "VALUES ($1, $2, $3) " +
+            "RETURNING favorito_plano_id";
+
+            console.log(utilizador_id +"|"+ plano_treino_id + "|" +  is_plano_favorito);
+        let result = await pool.query(sql, [utilizador_id, plano_treino_id, is_plano_favorito]);
+        let planoooo = result.rows[0].favorito_plano_id;
+        return { status: 200, data: planoooo };
+    } catch (err) {
+        console.log(err);
+        if (err.errno == 23503) // FK error
+            return { status: 400, data: { msg: "Type not found" } };
+        else
+            return { status: 500, data: err };
+    }
+}
--- routes/planos.js
+++ routes/planos.js
@@ -2,4 +2,51 @@ var express = require('express');
 var router = express.Router();
 var planosModel = require('../models/planosModel');
 
+//GET DE UM PLANO ESPECIFICO
 
+router.get('/:idplano', async function(req, res, next){
+
+    let id_plano = req.params.idplano;
+    console.log("[planosRoutes] Retrieving plan from with ID: " + id_plano);
+    let result = await planosModel.getPlanoById(id_plano);
+    res.status(result.status).send(result.data);
+  
+  });
+
+
+  router.get('/planouser/:idutilizador', async function(req, res, next){
+
+    let id_utilizador = req.params.idutilizador;
+    console.log("[planosRoutes] Retrieving planos from user " + id_utilizador);
+    let result = await planosModel.getPlanosUser(id_utilizador);
+    res.status(result.status).send(result.data);
+  
+  });
+
+
+  router.post('/insertnewplano', async function(req, res, next) {
+    let newPlano = req.body;
+    console.log("[planosRoutes] Saving plano " + JSON.stringify(newPlano));
+    let result = await planosModel.savePlano(newPlano);
+    res.sendStatus(result.status).send(result.data);
+  });
+
+  router.get('/getexercises/:idplanotreino', async function(req, res, next){
+  
+    let id_plano = req.params.idplanotreino;
+   
+    let result = await planosModel.getPlanosExercises(id_plano);
+    res.status(result.status).send(result.data);
+
+  });
+
+  router.post('/insertnewfavoriteplano', async function(req, res, next) {
+    let newPlano = req.body;
+    console.log("[planosRoutes] Saving plano " + JSON.stringify(newPlano));
+    let result = await planosModel.savePlanoFavorite(newPlano);
+    res.sendStatus(result.status).send(result.data);
+  });
+
+  
+
+  module.exports = router;
