5f9b828e8bc5e1e15b4d33906c3e850baaaf3622
Miguel Cruz <48087662+Miguelkrugi@users.noreply.github.com>
2022-05-13 09:34:55 +0100

    ch116

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -33,7 +33,7 @@ window.onload = async function(){
 
     x.getCurrentPosition(initMap, failure); //Obter localizacao atual
 
-    window.alert("CALLING INITMAP()")
+    //window.alert("CALLING INITMAP()")
     initMap();
 }
 
@@ -66,16 +66,45 @@ async function initMap(position){
             latLngBounds: PORTUGAL_MAPBOUNDS,
             strictBounds: false,
 
-        }
+        },
+        disableDefaultUI: true, //Remove os controles default de um mapa Google | Formatação
 
     }
 
     var map = new google.maps.Map(document.getElementById('map'), options); //VARIAVEL QUE ARMAZENA O MAPA
 
+    //INPUT DO GEOCODER
+
+    const defaultBoundsPlaces = { //Mostrar locais com proximidade | Definição de um raio á posição atual do utilizador
+
+       north: options.center.lat + 2.5,
+       south: options.center.lat - 2.5,
+       east: options.center.lng + 2.5,
+       west: options.center.lng - 2.5,
+
+    }
+
+
+
+    const options2 = {
+
+        //Restrições do aparecimento de locais
+
+        componentRestrictions: {country: "pt"}, //Autocomplete somente com estabelecimentos situados em PORTUGAL.
+        fields: ["address_components", "geometry", "icon", "name"], //Dados que queremos obter (morada, nome, geometria -> coordenadas e icone)
+        strictBounds: false,
+        types: ["establishment"], //Obter locais de estabelecimentos
+        bounds: defaultBoundsPlaces,
+
+    };
+
+    const autocomplete2 = new google.maps.places.Autocomplete(document.getElementById("input"), options2);
+
+    
     
     try{
 
-        window.alert("SUCESS GETTING INFO")
+       // window.alert("SUCESS GETTING INFO")
  
         let place = await $.ajax({
   
