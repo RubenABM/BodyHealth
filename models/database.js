021acb3c3f43a8badc2e05624b8b0a21017479de
RubenABM <rubenbatistamartins@gmail.com>
2022-06-07 03:12:55 +0100

    Update database.js

--- models/database.js
+++ models/database.js
@@ -2,7 +2,7 @@
 
 var pg = require('pg');
 
-const connectionString = "postgres://ztvljuubrwqlbj:6526b8c1fc57028302754761c52b50d7458c89db5987a09cbf74a5aefbe94573@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d5rohblj5db5qm"
+const connectionString = "postgres://rseyionrdlqafm:84d61f6abfa8c910d9bbc729c05eae094bf605779341b4c23c3435a825bbe9ec@ec2-63-34-180-86.eu-west-1.compute.amazonaws.com:5432/dflfor52b1lqbq"
 //heroku:
 //"postgres://zrznopdgddhzpb:c25ef89e544d07ff6382509ce1ec20a4535bffede8a2d96367c35ec1910e88b2@ec2-52-212-228-71.eu-west-1.compute.amazonaws.com:5432/d8sb04nd3dc7fk"
 //"postgres://NomeUtilizador:password@localhost:5432/students"
