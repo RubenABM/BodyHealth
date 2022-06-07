94918ce2949e3895d6a901f1f61c439e7403ae9b
RubenABM <rubenbatistamartins@gmail.com>
2022-06-08 00:36:58 +0100

    123

--- public/javascripts/createdreceitasementas.js
+++ public/javascripts/createdreceitasementas.js
@@ -41,7 +41,7 @@ async function getEmentas(){
 
 function createementaHTML(meal){
   
-  return "<div class='selectbox5' id='selectbox55'>" + "<p name='criador1' id='criador1' style='text-align: center; font-size: 90%; margin-top: 10%;'>" + meal.user_name +"</p>" + "<h2 style='color: white; font-size: 90%; margin-top: 1%; position: absolute;'>" + meal.ementa_titulo + "</h2>" + "<hr id ='divisorBoxes' style = 'margin-top: 50%;'>" + "</hr>" + "<h2 style='color: white; font-size: 90%;'>" + meal.basee_nome + "</h2>" + "<h2 style='color: white; font-size: 90%;'>" + meal.ementa_categoria_nome + "</h2>" + "<button style='background-color: #e8bf5c;' onclick='editarmeal(" + JSON.stringify(meal) + ")'>EDITAR</button>" + "</div>"
+  return "<div class='selectbox' id='selectbox55'>" + "<p name='criador1' id='criador1' style='text-align: center; font-size: 90%; margin-top: 5%; color:black'>" + meal.user_name +"</p>" + "<h2 style='font-size: 90%; margin-top: 5%;'>" + meal.ementa_titulo + "</h2>" + "<hr id ='divisorBoxes' style = 'margin-top: 15%;'>" + "</hr>" + "<h2 style='font-size: 90%;'>" + meal.basee_nome + "</h2>" + "<h2 style='font-size: 90%;'>" + meal.ementa_categoria_nome + "</h2>" + "<button style='background-color: #e8bf5c;' onclick='editarmeal(" + JSON.stringify(meal) + ")'>EDITAR</button>" + "</div>"
 
   /*<p name="criador1" id="criador1" style="text-align: center;font-size: 90%; margin-top: 2%;">CRIADOR DA
   RECEITA
@@ -462,7 +462,7 @@ async function filterEmentasCategory(type){
 
 function createrecipeHTML(recipe){
   
-  return "<div class='selectbox6' id='selectbox55'>" + "<p name='criador1' id='criador1' style='text-align: center; font-size: 90%; margin-top: 5%; color:black;'>" + recipe.user_name +"</p>" + "<h2 style='font-size: 90%; margin-top: 1%; position: absolute;'>" + recipe.receita_titulo + "</h2>" + "<hr id ='divisorBoxes' style = 'margin-top: 50%;'>" + "</hr>" + "<h2 style='font-size: 90%;'>" + recipe.basee_nome + "</h2>" + "<h2 style='font-size: 90%;'>" + recipe.receita_categoria_nome + "</h2>" + "</div>"
+  return "<div class='selectbox' id='selectbox55'>" + "<p name='criador1' id='criador1' style='text-align: center; font-size: 90%; margin-top: 5%; color:black;'>" + recipe.user_name +"</p>" + "<h2 style='font-size: 90%; margin-top: 5%; margin-left: 2%;'>" + recipe.receita_titulo + "</h2>" + "<hr id ='divisorBoxes' style = 'margin-top: 25%;'>" + "</hr>" + "<h2 style='font-size: 90%; margin-left: 2%;'>" + recipe.basee_nome + "</h2>" + "<h2 style='font-size: 90%; margin-left: 2%;'>" + recipe.receita_categoria_nome + "</h2>" + "</div>"
  // return "<div class='selectbox5' id='selectbox55'>" + recipe.receita_titulo + "</div>";
 
   /*<p name="criador1" id="criador1" style="text-align: center;font-size: 90%; margin-top: 2%;">CRIADOR DA
